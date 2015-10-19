0dc61a8f9d129f429510faa646f1f9aa6aa84c8b
Dimitris Kardarakos <dimkard@gmail.com>
2015-10-19 16:20:56 +0300

    Update packaging (locale)

--- kNewHostStuff.sh
+++ kNewHostStuff.sh
@@ -3,12 +3,11 @@ mkdir -p tempstuff/package/contents
 #copy data
 cp -R package/contents/* tempstuff/package/contents/
 cp changelog.txt README LICENSE.GPL3 hablamemo-icon.svg package/metadata.desktop tempstuff/package/ 
-#TODO: RECOVER icon
-
-#create locale //TODO:RECOVER
-#mkdir -p tempstuff/distrowatcher/contents/locale/el/LC_MESSAGES
-#cp build/el.gmo tempstuff/distrowatcher/contents/locale/el/LC_MESSAGES/org.kde.distrowatcher.mo
-#cp po/el.po tempstuff/distrowatcher/contents/locale/el/LC_MESSAGES/org.kde.distrowatcher.po
+#create locale
+mkdir -p tempstuff/package/contents/locale/el/LC_MESSAGES
+msgfmt po/el.po --output-file po/el.mo
+cp po/el.mo tempstuff/package/contents/locale/el/LC_MESSAGES/org.kde.hablamemo.mo
+cp po/el.po tempstuff/package/contents/locale/el/LC_MESSAGES/org.kde.hablamemo.po
 pushd tempstuff
 zip -r ../hablamemo.plasmoid package/* 
 popd
