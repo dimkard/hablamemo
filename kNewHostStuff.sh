510cb1f0b55b56414aae492ef0085aedb81ebb7f
Dimitris Kardarakos <dimkard@gmail.com>
2015-10-25 11:53:17 +0200

    Localized file name change

--- kNewHostStuff.sh
+++ kNewHostStuff.sh
@@ -6,8 +6,8 @@ cp changelog.txt README LICENSE.GPL3 hablamemo-icon.svg package/metadata.desktop
 #create locale
 mkdir -p tempstuff/package/contents/locale/el/LC_MESSAGES
 msgfmt po/el.po --output-file po/el.mo
-cp po/el.mo tempstuff/package/contents/locale/el/LC_MESSAGES/org.kde.hablamemo.mo
-cp po/el.po tempstuff/package/contents/locale/el/LC_MESSAGES/org.kde.hablamemo.po
+cp po/el.mo tempstuff/package/contents/locale/el/LC_MESSAGES/plasma_applet_org.kde.hablamemo.mo
+#cp po/el.po tempstuff/package/contents/locale/el/LC_MESSAGES/org.kde.hablamemo.po
 pushd tempstuff
 zip -r ../hablamemo.plasmoid package/* 
 popd
