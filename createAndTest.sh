7e8bd10b8d21d15916fcdcf95b10d60d2a4d63be
Dimitris Kardarakos <dimkard@gmail.com>
2017-03-15 12:54:57 +0200

    Update remove statement

--- createAndTest.sh
+++ createAndTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm hablamemo.plasmoid
-plasmapkg2 --remove org.kde.hablamemo
+plasmapkg2 --remove  ~/.local/share/plasma/plasmoids/org.kde.hablamemo/
 ./kNewHostStuff.sh
 plasmapkg2 --install hablamemo.plasmoid 
 plasmawindowed org.kde.hablamemo
